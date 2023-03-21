50def6dfcd81cbe80b6f1060650666ee64016650
Vauff <mctehkitti@gmail.com>
2023-03-21 18:17:44 -0400

    Add lastUpdated field to map images API

--- mapimgs/list.php
+++ mapimgs/list.php
@@ -1,16 +1,25 @@
 <?php
 $folders = glob("*", GLOB_ONLYDIR);
+$latest_ctime = 0;
 $fullObject = [];
 
 foreach ($folders as $folder)
 {
 	$arrFiles = glob($folder . "/*.jpg");
+
+	foreach ($arrFiles as $file)
+	{
+		if (filectime($file) > $latest_ctime)
+			$latest_ctime = filectime($file);
+	}
+
 	$arrFiles = str_replace($folder . "/", "", $arrFiles);
 	$arrFiles = str_replace(".jpg", "", $arrFiles);
 
 	$fullObject[$folder] = $arrFiles;
 }
 
-header('Content-Type: application/json');
+$fullObject["lastUpdated"] = $latest_ctime;
+header("Content-Type: application/json");
 echo json_encode($fullObject, JSON_PRETTY_PRINT);
 ?>
