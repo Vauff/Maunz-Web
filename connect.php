fc5e745461f20518aecaf6625212e4e5ef347575
Vauff <mctehkitti@gmail.com>
2025-07-18 16:58:21 -0400

    Update connect.php to resolve domain names
    Another workaround on top of the workaround. Since Steam has stopped opening domain connect links for some reason
    
    Thanks @.rushaway

--- connect.php
+++ connect.php
@@ -1,6 +1,27 @@
 <?php
-if ($_GET["ip"])
-	header("Location: steam://connect/" . $_GET["ip"]);
-else
-	echo "<h2>You need to specify the \"ip\" query parameter!</h2>"
+	if (!isset($_GET['ip']))
+		die("You need to specify the \"ip\" query parameter");
+
+	$input = $_GET['ip'];
+
+	if (strpos($input, ':') !== false)
+		list($host, $port) = explode(':', $input, 2);
+	else
+		die("Invalid address format");
+
+	if (filter_var($host, FILTER_VALIDATE_IP))
+	{
+		$ip = $host;
+	}
+	else
+	{
+		$ip = gethostbyname($host);
+
+		if ($ip === $host)
+			die("Unable to resolve domain name");
+	}
+
+	$connect = "steam://connect/" . $ip . ":" . $port;
+	header('Location: ' . $connect);
+	exit();
 ?>
